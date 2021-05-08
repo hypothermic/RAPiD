dc8cb765898c241a76b8cc83e269979e1f6318c1
hypothermic <admin@hypothermic.nl>
2021-05-08 02:00:22 +0200

    Read camera IP address from socket

--- vps.py
+++ vps.py
@@ -9,14 +9,14 @@ import selectors
 import socket
 import types
 
-HOST = "0.0.0.0"
-PORT = 40001
+LISTEN_HOST = "0.0.0.0"
+LISTEN_PORT = 40001
 
 # Initialize detector
 #detector = Detector(model_name='rapid', weights_path='./weights/pL1_MWHB1024_Mar11_4000.ckpt')
 
 # A simple example to run on a single image and plt.imshow() it
-image = detector.detect_one(img_path='./images/exhibition.jpg', input_size=1024, conf_thres=0.3, visualize=False, return_img=True)
+#image = detector.detect_one(img_path='./images/exhibition.jpg', input_size=1024, conf_thres=0.3, visualize=False, return_img=True)
 
 #im = Image.fromarray(image)
 #im.save("output.jpg")
@@ -24,11 +24,25 @@ image = detector.detect_one(img_path='./images/exhibition.jpg', input_size=1024,
 selector = selectors.DefaultSelector()
 listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind((HOST, PORT))
+listen_addr = (LISTEN_HOST, LISTEN_PORT)
+listener.bind(listen_addr)
 listener.listen()
-print("Wachten op inkomende connecties op", (HOST, PORT))
+print("Wachten op inkomende connecties op:", listen_addr)
 listener.setblocking(False)
 selector.register(listener, selectors.EVENT_READ, data=None)
+
+def read_net_command_connect(socket, data):
+	length = socket.recv(1)
+	cam_ip = socket.recv(length[0]).decode("utf-8")
+	print("Read camera ip:", cam_ip)
+	data.cam_ip = cam_ip
+
+def read_net_command(command, socket, data):
+	commands = {
+		b'\x01' : read_net_command_connect
+	}
+
+	commands[command](socket, data)
 
 def accept_client(socket):
 	connection, address = socket.accept()
@@ -43,10 +57,11 @@ def read_client(key, mask):
 	data = key.data
 	
 	if mask & selectors.EVENT_READ:
-		recv_data = socket.recv(1)
-		if recv_data:
-			print("Received", repr(recv_data))
-			# TODO react to command
+		command = socket.recv(1)
+		if command:
+			print("Handling command:", repr(command))
+			read_net_command(command, socket, data)
+
 	if mask & selectors.EVENT_WRITE:
 		if data.outgoing:
 			print("Writing", repr(data.outgoing))
